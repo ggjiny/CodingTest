0ff90716c3f3c790f376f5c98c94d906be560860
Yaejin Kim <71435571+ggjiny@users.noreply.github.com>
2024-02-14 03:32:08 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,5 +1,5 @@
 function solution(s) {
-    let arr = s.split(" ").sort((a,b)=>a-b).map(Number)
+    let arr = s.split(" ")//.sort((a,b)=>a-b).map(Number)
     let min = Math.min(...arr);
     let max = Math.max(...arr);
     return `${min} ${max}`
