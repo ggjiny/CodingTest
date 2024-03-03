2e961cac5cc8df87fd811388e798e18f0088821f
Yaejin Kim <71435571+ggjiny@users.noreply.github.com>
2024-03-04 00:19:35 +0900

    [level 2] Title: 피보나치 수, Time: 4.58 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,11 +1,8 @@
 
 function solution(n) {
-    function fibo(n){
-        let arr=[0,1]
-        for(let i=0; i<n-1;i++){
-            arr.push((arr[i]+arr[i+1])%1234567)
-        }
-        return arr[arr.length-1]
+    let arr=[0,1]
+    for(let i=0; i<n-1;i++){
+        arr.push((arr[i]+arr[i+1])%1234567);
     }   
-    return fibo(n)
+    return arr[arr.length-1]
 }
